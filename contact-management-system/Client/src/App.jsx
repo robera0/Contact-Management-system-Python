4ec24695075c9123efd72ec773925defe2e8d3fd
robra0 <ararsarobra@gamil.com>
2025-10-29 00:03:32 +0300

    Initial commit

---
+++ contact-management-system/Client/src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./Components/Home";
+
+const App=()=>{
+
+  return(
+     <>
+     <Home/>
+     </>
+  )
+  
+}
+
+export default App
---
+++ contact-management-system/Client/src/Components/Home.jsx
@@ -0,0 +1,44 @@
+import { Plus,Trash } from 'lucide-react';
+
+const Home = () => {
+  return (
+    <div className='flex justify-center mt-8'>
+        <div className='w-[50%] h-full flex flex-col justify-center items-center gap-12  rounded-xl shadow-xl pt-8'>
+            <div>
+                 <h1 className='text-xl font-bold text-center'>Contact Management System</h1>
+            </div>
+            <div className='w-[80%]  flex justify-between'>
+              <div>
+                <button className='flex justify-center items-center w-24 h-10 gap-2 bg-green-400 cursor-pointer rounded'>
+                   <Plus strokeWidth={3} className='w-5 h-5 text-white'/>
+                <span className='text-white font-semibold'>Add</span>
+                </button>
+                
+             </div>
+
+                 <div>
+                <button className='flex justify-center items-center w-24 h-10  gap-2 bg-red-600 cursor-pointer rounded'>
+                   <Trash strokeWidth={3} className='w-5 h-5 text-white'/>
+                <span className='text-white font-semibold'>Delete</span>
+                </button>
+                
+             </div>
+            </div>
+
+            {/*todo list */}
+
+             <div className='w-[90%] h-100 mb-12 flex flex-col pt-6 pl-4 bg-[#222222] text-white  gap-12  rounded-xl shadow-sm'>
+                 <h1 className='font-bold'>Contact Management System</h1>
+             </div>
+
+
+         
+        </div>
+       
+      
+        
+    </div>
+  )
+}
+
+export default Home
